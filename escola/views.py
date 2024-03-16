49051fb94e460b4a374575b4f1c6571d9c742d0d
Ana Paula Rabelo <prabelo79@gmail.com>
2024-03-16 18:15:49 -0300

    add: cria modelos da API
    Adiciona pacotes Django Rest Framework e Markdown, cria modelos Aluno e Curso, e adiciona rota para listar alunos

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def alunos(request):
+    if request.method == 'GET':
+        alunos = [
+            {'id': 1, 'nome': 'Eduardo'},
+            {'id': 2, 'nome': 'Maria'},
+            {'id': 3, 'nome': 'Ana'},
+        ]
+        return JsonResponse({'alunos': alunos})
--- setup/settings.py
+++ setup/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',  # Adicionado para uso do Django Rest Framework  
+    'escola',  # Adicionado para uso do app escola
 ]
 
 MIDDLEWARE = [
